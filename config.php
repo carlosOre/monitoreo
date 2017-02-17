4efeda85e7c9a007d0274bc2e7a6ac0999931f30
Orellana Carlos <axis.noclip@gmail.com>
2017-02-16 22:18:37 -0300

    Primer commit, todo el preyecto

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+    // config.php
+    // Credenciales
+    $dbhost = "localhost";
+    $dbuser = "monitoreo";
+    $dbpass = "qweasdqwza";
+    $dbname = "sensores";
+    // Conexión con la base de datos
+    $con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+?>
